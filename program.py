a5fffc4a7cf501ea78b97851ac64436784cf53e5
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-10-22 14:43:48 +0000

    rounded

--- program.py
+++ program.py
@@ -7,42 +7,58 @@ if pizza == "Large":
     toppings = input()
     if toppings == "1":
         subtotal = float(large + 1.00)
+        import math
+        subtotal = math.floor(subtotal * 100) / 100
         print("Your subtotal is")
         print("$", subtotal)
         tax = float(0.13 * subtotal)
+        tax = math.floor(tax * 100) / 100
         print("Your tax is")
         print("$", tax)
         fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
         print("Your final cost is")
         print("$", fintot)
     if toppings == "2":
         subtotal = float(large + 1.75)
+        import math
+        subtotal = math.floor(subtotal * 100) / 100
         print("Your subtotal is")
         print("$", subtotal)
         tax = float(0.13 * subtotal)
+        tax = math.floor(tax * 100) / 100
         print("Your tax is")
         print("$", tax)
         fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
         print("Your final cost is")
         print("$", fintot)
     if toppings == "3":
         subtotal = float(large + 2.50)
+        import math
+        subtotal = math.floor(subtotal * 100) / 100
         print("Your subtotal is")
         print("$", subtotal)
         tax = float(0.13 * subtotal)
+        tax = math.floor(tax * 100) / 100
         print("Your tax is")
         print("$", tax)
         fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
         print("Your final cost is")
         print("$", fintot)
     if toppings == "4":
         subtotal = float(large + 3.35)
+        import math
+        subtotal = math.floor(subtotal * 100) / 100
         print("Your subtotal is")
         print("$", subtotal)
         tax = float(0.13 * subtotal)
+        tax = math.floor(tax * 100) / 100
         print("Your tax is")
         print("$", tax)
         fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
         print("Your final cost is")
         print("$", fintot)
     
@@ -53,41 +69,57 @@ if pizza == "Extra Large":
     toppings = input()
     if toppings == "1":
         subtotal = float(extralarge + 1.00)
+        import math
+        subtotal = math.floor(subtotal * 100) / 100
         print("Your subtotal is")
         print("$", subtotal)
         tax = float(0.13 * subtotal)
+        tax = math.floor(tax * 100) / 100
         print("Your tax is")
         print("$", tax)
         fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
         print("Your final cost is")
         print("$", fintot)
     if toppings == "2":
         subtotal = float(extralarge + 1.75)
+        import math
+        subtotal = math.floor(subtotal * 100) / 100
         print("Your subtotal is")
         print("$", subtotal)
         tax = float(0.13 * subtotal)
+        tax = math.floor(tax * 100) / 100
         print("Your tax is")
         print("$", tax)
         fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
         print("Your final cost is")
         print("$", fintot)
     if toppings == "3":
         subtotal = float(extralarge + 2.50)
+        import math
+        subtotal = math.floor(subtotal * 100) / 100
         print("Your subtotal is")
         print("$", subtotal)
         tax = float(0.13 * subtotal)
+        tax = math.floor(tax * 100) / 100
         print("Your tax is")
         print("$", tax)
         fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
         print("Your final cost is")
         print("$", fintot)
     if toppings == "4":
         subtotal = float(extralarge + 3.35)
+        import math
+        subtotal = math.floor(subtotal * 100) / 100
         print("Your subtotal is")
         print("$", subtotal)
         tax = float(0.13 * subtotal)
+        tax = math.floor(tax * 100) / 100
         print("Your tax is")
         print("$", tax)
         fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
         print("Your final cost is")
         print("$", fintot)
