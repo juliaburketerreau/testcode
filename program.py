314266cb15927ef3d731e178d0f42d86ec181849
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-10-23 14:47:30 +0000

    added 0

--- program.py
+++ program.py
@@ -5,6 +5,19 @@ if pizza == "Large":
     large = 6
     print("How many toppings, 1, 2, 3 or 4?")
     toppings = input()
+    if toppings == "0":
+        subtotal = float(large)
+        import math
+        print("Your subtotal is")
+        print("$", subtotal)
+        tax = float(large * 0.13)
+        tax = math.floor(tax * 100) / 100
+        print("Your tax is")
+        print("$", tax)
+        fintot = float(tax + subtotal)
+        fintot = math.floor(fintot * 100) / 100
+        print("Your final cost is")
+        print("$", fintot)
     if toppings == "1":
         subtotal = float(large + 1.00)
         import math
